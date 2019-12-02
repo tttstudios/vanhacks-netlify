3df4ea087b906ac3454f6ea87685cf11545929e0
jae3choi <jae.choi@ttt.studio>
2019-12-02 12:06:58 -0800

    Add key tag

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -11,7 +11,7 @@ export default function Schedule({ data }) {
                     <div className="row">
                         {data.days.map((d, index) => {
                             return (
-                                <div className="col-md-4">
+                                <div key={index} className="col-md-4">
                                     <div className="col schedule">
                                         <h4 className="date">{d.day}</h4>
                                         {
