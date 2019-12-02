50443154dcfed9f097af3404fd22c23b3a55b498
jae3choi <jae.choi@ttt.studio>
2019-12-02 12:34:07 -0800

    Fix mentor value issue

--- src/components/Mentors.js
+++ src/components/Mentors.js
@@ -16,26 +16,26 @@ export default function Mentors() {
                             target="_blank" noValidate>
                             <div id="mc_embed_signup_scroll">
                                 <div className="mc-field-group">
-                                    <input type="text" value="" name="FNAME" className="required mentor-form-input"
+                                    <input type="text" defaultValue="" name="FNAME" className="required mentor-form-input"
                                         id="mce-FNAME" placeholder="First Name" required />
                                 </div>
                                 <div className="mc-field-group">
-                                    <input type="text" value="" name="LNAME" className="required mentor-form-input"
+                                    <input type="text" defaultValue="" name="LNAME" className="required mentor-form-input"
                                         id="mce-LNAME" placeholder="Last Name" required />
                                 </div>
                                 <div className="mc-field-group">
-                                    <input type="email" value="" name="EMAIL" className="required mentor-form-input"
+                                    <input type="email" defaultValue="" name="EMAIL" className="required mentor-form-input"
                                         id="mce-EMAIL" placeholder="Email Address" required />
                                 </div>
                                 <div className="mc-field-group">
-                                    <input type="text" value="" name="EXPERTISE" className="required mentor-form-input"
+                                    <input type="text" defaultValue="" name="EXPERTISE" className="required mentor-form-input"
                                         id="mce-EXPERTISE" placeholder="Area of Expertise" required />
                                 </div>
                                 <div style={{ display: 'none' }} className="mc-field-group input-group">
                                     <strong>Mentor?</strong>
                                     <ul>
                                         <li>
-                                            <input type="checkbox" value="1" name="group[24713][1]"
+                                            <input type="checkbox" defaultValue="1" name="group[24713][1]"
                                                 id="mce-group[24713]-24713-0" checked />
                                             <label htmlFor="mce-group[24713]-24713-0">mentor</label>
                                         </li>
@@ -46,7 +46,7 @@ export default function Mentors() {
                                     <div className="response" id="mce-success-response" style={{ display: 'none' }}></div>
                                 </div>
                                 {/* <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups--> */}
-                                <div style={{ position: 'absolute; left: -5000px' }} aria-hidden="true"><input type="text"
+                                <div style={{ position: 'absolute', left: '-5000px' }} aria-hidden="true"><input type="text"
                                     name="b_5907e3e94aaabdaf3a29b7f4c_bf87b78c48" tabIndex="-1" value="" /></div>
                                 <div className="clear"><input type="submit" value="Sign up" name="signup"
                                     id="mc-embedded-subscribe" className="button-yellow contact-email-button"
