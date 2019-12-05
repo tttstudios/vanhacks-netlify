3bdc1bdd1df7d6bfbd25afdfb5b2e2e49a9e3fa6
jae3choi <jae.choi@ttt.studio>
2019-12-04 16:29:00 -0800

    Working navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,60 +1,83 @@
 import Head from 'next/head';
+import { Component } from 'react';
 
-const Navbar = () => (
+export default class Navbar extends Component {
+    constructor() {
+        super();
+        this.state = {
+            activeClass: 'navbar-normal'
+        }
+    }
+    componentDidMount() {
+        window.addEventListener('scroll', () => {
+            let activeClass = 'navbar-normal';
+            if (window.scrollY >= 100) {
+                activeClass = 'navbar-shrink';
+            }
+            this.setState({ activeClass });
+        });
+    }
 
-    /* <!-- Navigation --> */
-    <nav className="navbar navbar-default navbar-fixed-top" >
-        <div className="container">
-            {/* <!-- Brand and toggle get grouped for better mobile display --> */}
-            <div className="navbar-header page-scroll">
-                <button type="button" className="navbar-toggle" data-toggle="collapse"
-                    data-target="#bs-example-navbar-collapse-1">
-                    <span className="sr-only">Toggle navigation</span>
-                    <span className="icon-bar"></span>
-                    <span className="icon-bar"></span>
-                    <span className="icon-bar"></span>
-                </button>
-                <a className="navbar-brand page-scroll" href="#page-top"></a>
-            </div>
+    componentWillUnmount() {
+        window.removeEventListener('scroll');
 
-            {/* <!-- Collect the nav links, forms, and other content for toggling --> */}
-            <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul className="nav navbar-nav navbar-right">
-                    <li className="hidden">
-                        <a href="#page-top"></a>
-                    </li>
+    }
+    render() {
 
-                    <li>
-                        <a className="page-scroll nav-link-font" href="#about">About</a>
-                    </li>
-                    <li>
-                        <a className="page-scroll nav-link-font" href="#schedule">Schedule</a>
-                    </li>
-                    <li>
-                        <a className="page-scroll nav-link-font" href="#sponsors">Sponsors</a>
-                    </li>
-                    <li>
-                        <a className="page-scroll nav-link-font" href="#judges">Judges</a>
-                    </li>
-                    <li>
-                        <a className="page-scroll nav-link-font" href="#hosts">Hosts</a>
-                    </li>
-                    <li>
-                        <a className="page-scroll nav-link-font" href="#mentors">Mentors</a>
-                    </li>
-                    <li>
-                        <a className="page-scroll nav-link-font" href="#info">Info</a>
-                    </li>
-                    <li>
-                        <a className="page-scroll nav-link-font" href="#contact">Contact</a>
-                    </li>
-                </ul>
-            </div>
-            {/* <!-- /.navbar-collapse --> */}
-        </div>
-        {/* <!-- /.container-fluid --> */}
-    </nav>
+        return (
 
-);
+            /* <!-- Navigation --> */
+            <nav className={`navbar navbar-default navbar-fixed-top ${this.state.activeClass}`} >
+                <div className="container">
+                    {/* <!-- Brand and toggle get grouped for better mobile display --> */}
+                    <div className="navbar-header page-scroll">
+                        <button type="button" className="navbar-toggle" data-toggle="collapse"
+                            data-target="#bs-example-navbar-collapse-1">
+                            <span className="sr-only">Toggle navigation</span>
+                            <span className="icon-bar"></span>
+                            <span className="icon-bar"></span>
+                            <span className="icon-bar"></span>
+                        </button>
+                        <a className="navbar-brand page-scroll" href="#page-top"></a>
+                    </div>
 
-export default Navbar;
+                    {/* <!-- Collect the nav links, forms, and other content for toggling --> */}
+                    <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <ul className="nav navbar-nav navbar-right">
+                            <li className="hidden">
+                                <a href="#page-top"></a>
+                            </li>
+
+                            <li>
+                                <a className="page-scroll nav-link-font" href="#about">About</a>
+                            </li>
+                            <li>
+                                <a className="page-scroll nav-link-font" href="#schedule">Schedule</a>
+                            </li>
+                            <li>
+                                <a className="page-scroll nav-link-font" href="#sponsors">Sponsors</a>
+                            </li>
+                            <li>
+                                <a className="page-scroll nav-link-font" href="#judges">Judges</a>
+                            </li>
+                            <li>
+                                <a className="page-scroll nav-link-font" href="#hosts">Hosts</a>
+                            </li>
+                            <li>
+                                <a className="page-scroll nav-link-font" href="#mentors">Mentors</a>
+                            </li>
+                            <li>
+                                <a className="page-scroll nav-link-font" href="#info">Info</a>
+                            </li>
+                            <li>
+                                <a className="page-scroll nav-link-font" href="#contact">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                    {/* <!-- /.navbar-collapse --> */}
+                </div>
+                {/* <!-- /.container-fluid --> */}
+            </nav>
+        )
+    }
+}
