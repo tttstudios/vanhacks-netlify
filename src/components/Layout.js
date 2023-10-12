00ec65525b4d1027148f8875a7c99b2bdb6aa758
gokul-ttt <gokul.kumar@ttt.studio>
2023-10-12 12:54:15 -0700

    add link to vanhack

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,7 +3,9 @@ import Navbar from './Navbar';
 const Layout = props => (
 
     <div>
-
+        <div style={{ width: '100%',  padding: '10px' }}>
+            "If you are looking for our friends at Vanhack… a great organization that helps companies hire tech professionals from a world of talent go to <a href="http://www.vanhack.com" target="_blank" rel="noopener noreferrer">http://www.vanhack.com</a>"
+        </div>
         <Navbar />
 
         {props.children}
